e208b4bedb74036e220c05870ce198b8aab78e05
heidilaursen <heidimlaursen@gmail.com>
2016-05-30 14:47:11 -0700

    updated readme, and server to add reductor component

---
+++ README.md
@@ -0,0 +1,13 @@
+###React Application with Flux
+
+## How to use
+  1 Clone repo and npm install all packages.
+  2 Bundle webpack with
+  ```
+  npm run dev
+  ```
+  3 Start server with
+  ```
+  npm start
+  ```
+  4 In your browswer go to 'localhost:3000' (or whatever your environmental variable is).
--- server.jsx
+++ server.jsx
@@ -26,45 +26,22 @@ app.use((req, res) => {
           <RoutingContext {...renderProps} />
       </Provider>
     );
-//     const InitialComponent = (
-//       <RoutingContext {...renderProps} />
-//     );
-//     const componentHTML = renderToString(InitialComponent);
-//     const HTML = `
-//     <!DOCTYPE html>
-//     <html>
-//       <head>
-//         <meta charset="utf-8">
-//         <title>Isomorphic Redux Demo</title>
-//       </head>
-//       <body>
-//         <div id="react-view">${componentHTML}</div>
-//         <script type="application/javascript" src="/bundle.js"></script>
-//       </body>
-//   </html>
-// `
-//     res.end(HTML);
+
+    const componentHTML = renderToString(InitialComponent);
+    const HTML = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>Isomorphic Redux Demo</title>
+      </head>
+      <body>
+        <div id="react-view">${componentHTML}</div>
+        <script type="application/javascript" src="/bundle.js"></script>
+      </body>
+  </html>
+`
+    res.end(HTML);
   });
 });
 export default app;
-
-// import express from 'express';
-// const app = express();
-// app.use((req, res) => {
-//   const HTML = `
-//   <!DOCTYPE html>
-//   <html>
-//     <head>
-//       <meta charset="utf-8">
-//       <title>Isomorphic Redux Demo</title>
-//     </head>
-//     <body>
-//       <div id="react-view"></div>
-//       <script type="application/javascript" src="/bundle.js"></script>
-//     </body>
-//   </html>
-//   `;
-//
-//   res.end(HTML);
-// });
-// export default app;
