2f249e3d358c8f3339186fcb1047808b3a35691f
heidilaursen <heidimlaursen@gmail.com>
2016-05-30 15:40:26 -0700

    adding lib and promise handling

--- server.jsx
+++ server.jsx
@@ -1,51 +1,77 @@
+
 import express                   from 'express';
 import React                     from 'react';
-import { renderToString }        from 'react-dom/server'
 import { RoutingContext, match } from 'react-router';
 import createLocation            from 'history/lib/createLocation';
 import routes                    from 'routes';
-import { createStore, combineReducers } from 'redux';
-import { Provider }                     from 'react-redux';
-import * as reducers                    from 'reducers';
+import { Provider }              from 'react-redux';
+import * as reducers             from 'reducers';
+import promiseMiddleware         from 'lib/promiseMiddleware';
+import fetchComponentData        from 'lib/fetchComponentData';
+import { createStore,
+         combineReducers,
+         applyMiddleware } from 'redux';
+
 const app = express();
 
-app.use((req, res) => {
+// So the example quote unquote 'production mode' works
+import fs from 'fs';
+app.use('/bundle.js', function (req, res) {
+  return fs.createReadStream('./dist/bundle.js').pipe(res);
+});
+
+app.use( (req, res) => {
   const location = createLocation(req.url);
   const reducer  = combineReducers(reducers);
-  const store    = createStore(reducer);
+  const store    = applyMiddleware(promiseMiddleware)(createStore)(reducer);
 
   match({ routes, location }, (err, redirectLocation, renderProps) => {
-    if (err) {
+    if(err) {
       console.error(err);
       return res.status(500).end('Internal server error');
     }
-    if (!renderProps) return res.status(404).end('Not found.');
 
-    const InitialComponent = (
-      <Provider store={store}>
-          <RoutingContext {...renderProps} />
-      </Provider>
-    );
-    const initalState = store.getState();
+    if(!renderProps)
+      return res.status(404).end('Not found');
 
-    const componentHTML = renderToString(InitialComponent);
-    const HTML = `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <meta charset="utf-8">
-        <title>Isomorphic Redux Demo</title>
-        <script type="application/javascript">
-          window.__INITIAL_STATE__ = ${JSON.stringify(initalState)};
-        </script> 
-      </head>
-      <body>
-        <div id="react-view">${componentHTML}</div>
-        <script type="application/javascript" src="/bundle.js"></script>
-      </body>
-  </html>
-`
-    res.end(HTML);
+    function renderView() {
+      const InitialView = (
+        <Provider store={store}>
+          {() =>
+            <RoutingContext {...renderProps} />
+          }
+        </Provider>
+      );
+
+      const componentHTML = React.renderToString(InitialView);
+
+      const initialState = store.getState();
+
+      const HTML = `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <meta charset="utf-8">
+          <title>Redux Demo</title>
+          <script>
+            window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
+          </script>
+        </head>
+        <body>
+          <div id="react-view">${componentHTML}</div>
+          <script type="application/javascript" src="/bundle.js"></script>
+        </body>
+      </html>
+      `;
+
+      return HTML;
+    }
+
+    fetchComponentData(store.dispatch, renderProps.components, renderProps.params)
+      .then(renderView)
+      .then(html => res.end(html))
+      .catch(err => res.end(err.message));
   });
 });
+
 export default app;
