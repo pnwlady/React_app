971c43fc0a03306eae393f978a157cc72d39df4d
heidilaursen <heidimlaursen@gmail.com>
2016-05-30 14:54:46 -0700

    add reducer inital state into client

--- client/index.jsx
+++ client/index.jsx
@@ -4,6 +4,17 @@ import { Router }  from 'react-router';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 import routes      from 'routes';
 const history = createBrowserHistory();
+let initalState = window.__INITIAL_STATE__;
+// Transform into Immutable.js collections,
+// but leave top level keys untouched for Redux
+Object
+  .keys(initialState)
+  .forEach(key => {
+    initialState[key] = fromJS(initialState[key]);
+   });
+const reducer = combineReducers(reducers);
+const store   = createStore(reducer, initialState);
+
 render(
   <Router children={routes} history={history} />,
   document.getElementById('react-view')
